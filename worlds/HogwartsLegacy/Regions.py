1453f684c9eb80e683cb51baf4a991cdeaa9488f
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2026-01-15 12:31:24 -0800

    Update Regions
    Updated Regions to use the Map Regions instead of the Seasons

--- worlds/HogwartsLegacy/Regions.py
+++ worlds/HogwartsLegacy/Regions.py
@@ -1,9 +1,26 @@
 from typing import Dict, List
 
 hogwarts_regions_all: Dict[str, List[str]] = {
-    "Menu" : ["Summer", "Fall", "Winter", "Spring"],
-    "Summer": [],
-    "Fall": [],
-    "Winter": [],
-    "Spring": []
+    "Menu" : [
+        "Tutorial",
+        "Hogwarts Castle",
+        "Hogsmeade"
+    ],
+    "Tutorial" : [],
+    "Hogwarts Castle" : [],
+    "Hogsmeade" : [],
+    "South Hogwarts Region" : [],
+    "North Hogwarts Region" : [],
+    "Hogsmeade Valley" : [],
+    "Forbidden Forest" : [],
+    "North Ford Bog" : [],
+    "Hogwarts Valley" : [],
+    "Feldcroft Region" : [],
+    "South Sea Bog" : [],
+    "Costal Cavern" : [],
+    "Poidsear Coast" : [],
+    "Marunweem Lake" : [],
+    "Cragcroftshire" : [],
+    "Manor Cape" : [],
+    "Clagmar Coast" : []
 }
