b5b949b95c6b59d9e16d05b150a28eaae5d8c5a6
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2026-01-15 13:50:16 -0800

    Update Locations.py
    Added Ancient Magic Hotspot Locations

--- worlds/HogwartsLegacy/Locations.py
+++ worlds/HogwartsLegacy/Locations.py
@@ -56,32 +56,46 @@ north_hogwarts_locations = [
     "The Collector’s Cave Floo Flame",
     "East North Hogwarts Region Floo Flame",
     "Forbidden Forest Floo Flame",
-    "Korrow Ruins Floo Flame"
+    "Korrow Ruins Floo Flame",
+    "North Hogwarts Region Ancient Magic Hotspot",
+
 ]
 
 hogsmeade_valley_locations = [
     "Upper Hogsfield Floo Flame",
     "East Hogsmeade Valley Floo Flame",
-    "Falbarton Castle Floo Flame"
+    "Falbarton Castle Floo Flame",
+    "Hogsmeade Valley Ancient Magic Hotspot",
+
 ]
 
 forbidden_forrest_locations = [
     "West Forbidden Forest Floo Flame",
-    "Jackdaw’s Tomb Floo Flame"
+    "Jackdaw’s Tomb Floo Flame",
+    "Forbidden Forest Ancient Magic Hotspot #1",
+    "Forbidden Forest Ancient Magic Hotspot #2",
+
 ]
 
 north_ford_bog_locations = [
-"Pitt-Upon-Ford Floo Flame",
-"North Ford Bog Floo Flame",
-"San Bakar’s Tower Floo Flame",
-"East North Ford Bog Floo Flame",
+    "Pitt-Upon-Ford Floo Flame",
+    "North Ford Bog Floo Flame",
+    "San Bakar’s Tower Floo Flame",
+    "East North Ford Bog Floo Flame",
 ]
 
 hogwarts_valley_locations = [
     "Central Hogwarts Valley Floo Flame",
     "Brocburrow Floo Flame",
     "Keenbridge Floo Flame",
-    "The Mine’s Eye Floo Flame"
+    "The Mine’s Eye Floo Flame",
+    "Hogwarts Valley Ancient Magic Hotspot #1",
+    "Hogwarts Valley Ancient Magic Hotspot #2",
+    "Hogwarts Valley Ancient Magic Hotspot #3",
+    "Hogwarts Valley Ancient Magic Hotspot #4",
+    "Hogwarts Valley Ancient Magic Hotspot #5",
+    "Hogwarts Valley Ancient Magic Hotspot #6",
+
 ]
 
 feldcroft_locations = [
@@ -91,7 +105,12 @@ feldcroft_locations = [
     "Feldcroft Floo Flame",
     "South Feldcroft Floo Flame",
     "Feldcroft Catacomb Floo Flame",
-    "Irondale Floo Flame"
+    "Irondale Floo Flame",
+    "Feldcroft Region Ancient Magic Hotspot #1",
+    "Feldcroft Region Ancient Magic Hotspot #2",
+    "Feldcroft Region Ancient Magic Hotspot #3",
+    "Feldcroft Region Ancient Magic Hotspot #4",
+
 ]
 
 south_sea_bog_locations = [
@@ -108,7 +127,10 @@ poidsear_coast_locations = [
     "Poidsear Castle Floo Flame",
     "Phoenix Mountain Cave Floo Flame",
     "South Poidsear Coast Floo Flame",
-    "Marunweem Bridge Floo Flame"
+    "Marunweem Bridge Floo Flame",
+    "Poidsear Coast Ancient Magic Hotspot #1",
+    "Poidsear Coast Ancient Magic Hotspot #2",
+
 ]
 
 marunweem_lake_locations = [
@@ -116,17 +138,24 @@ marunweem_lake_locations = [
     "Tower Tunnel Floo Flame",
     "Marunweem Lake Floo Flame",
     "Marunweem Floo Flame",
-    "Marunweem Ruins Floo Flame"
+    "Marunweem Ruins Floo Flame",
+    "Marunweem Lake Ancient Magic Hotspot #1",
+    "Marunweem Lake Ancient Magic Hotspot #2",
+
 ]
 
 cragcroftshire_locations = [
     "Cragcroft Shore Floo Flame",
-    "Cragcroft Floo Flame"
+    "Cragcroft Floo Flame",
+    "Cragcroftshire Ancient Magic Hotspot",
+
 ]
 
 manor_cape_locations = [
     "Bainburgh Floo Flame",
-    "West Manor Cape Floo Flame"
+    "West Manor Cape Floo Flame",
+    "Manor Cape Ancient Magic Hotspot",
+
 ]
 
 clagmar_coast_locations = [
