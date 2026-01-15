4e996010183b862feb74a1dc453b5641d8002600
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2026-01-15 11:19:27 -0800

    Added Region list to Regions.py
    Default regions.

---
+++ worlds/HogwartsLegacy/Regions.py
@@ -0,0 +1,9 @@
+from typing import Dict, List
+
+hogwarts_regions_all: Dict[str, List[str]] = {
+    "Menu" : ["Summer", "Fall", "Winter", "Spring"],
+    "Summer": [],
+    "Fall": [],
+    "Winter": [],
+    "Spring": []
+}
