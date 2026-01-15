d505d6514ad18c14b8d2de1a442f5b12f01ad669
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2026-01-15 13:31:51 -0800

    Added Floo Flame locations
    Added Floo Flame locations to each region

---
+++ worlds/HogwartsLegacy/Locations.py
@@ -0,0 +1,130 @@
+from BaseClasses import Location
+
+
+class HogwartsLegacyLocation(Location):
+    game: str = "Hogwarts Legacy"
+
+hogwarts_castle_locations = [
+    "Astronomy Tower Floo Flame",
+    "Charms Classroom Floo Flame",
+    "Defence Against the Dark Arts Tower Floo Flame",
+    "Defense Against the Dark Arts Classroom Floo Flame",
+    "Professor Fig’s Classroom Floo Flame",
+    "Transfiguration Classroom Floo Flame",
+    "Transfiguration Courtyard Floo Flame",
+    "Bell Tower Courtyard Floo Flame",
+    "Flying Class Lawn Floo Flame",
+    "Beasts Classroom Floo Flame",
+    "Hogwarts North Exit Floo Flame",
+    "West Tower Floo Flame",
+    "Grand Staircase Floo Flame",
+    "Grand Staircase Tower Floo Flame",
+    "Lower Grand Staircase Floo Flame",
+    "Quad Courtyard Floo Flame",
+    "Ravenclaw Tower Floo Flame",
+    "Trophy Room Floo Flame",
+    "Boathouse Floo Flame",
+    "Great Hall Floo Flame",
+    "Viaduct Courtyard Floo Flame",
+    "Central Hall Floo Flame",
+    "Divination Classroom Floo Flame",
+    "Greenhouses Floo Flame",
+    "Library Floo Flame",
+    "Potions Classroom Floo Flame",
+    "The Map Chamber Floo Flame",
+    "Room of Requirement Floo Flame",
+    "Clock Tower Courtyard Floo Flame",
+    "Faculty Tower Floo Flame",
+    "Gryffindor Common Room Floo Flame",
+    "Hospital Wing Floo Flame", # TODO: Double check if accessible
+    "Hogwarts South Exit Floo Flame"
+]
+
+hogmseade_locations = [
+    "South Hogsmeade Floo Flame",
+    "West Hogsmeade Floo Flame",
+    "North Hogsmeade Floo Flame"
+]
+
+south_hogwarts_locations = [
+    "Mooncalf Den Floo Flame",
+    "Aranshire Floo Flame",
+    "Lower Hogsfield Floo Flame"
+]
+
+north_hogwarts_locations = [
+    "The Collector’s Cave Floo Flame",
+    "East North Hogwarts Region Floo Flame",
+    "Forbidden Forest Floo Flame",
+    "Korrow Ruins Floo Flame"
+]
+
+hogsmeade_valley_locations = [
+    "Upper Hogsfield Floo Flame",
+    "East Hogsmeade Valley Floo Flame",
+    "Falbarton Castle Floo Flame"
+]
+
+forbidden_forrest_locations = [
+    "West Forbidden Forest Floo Flame",
+    "Jackdaw’s Tomb Floo Flame"
+]
+
+north_ford_bog_locations = []
+
+hogwarts_valley_locations = [
+    "Central Hogwarts Valley Floo Flame",
+    "Brocburrow Floo Flame",
+    "Keenbridge Floo Flame",
+    "The Mine’s Eye Floo Flame"
+]
+
+feldcroft_locations = [
+    "West Hogwarts Valley Floo Flame",
+    "North Feldcroft Floo Flame",
+    "Rookwood Castle Floo Flame",
+    "Feldcroft Floo Flame",
+    "South Feldcroft Floo Flame",
+    "Feldcroft Catacomb Floo Flame",
+    "Irondale Floo Flame"
+]
+
+south_sea_bog_locations = [
+    "Northern South Sea Bog Floo Flame"
+]
+
+costal_cavern_locations = [
+    "East South Sea Bog Floo Flame",
+    "North Poidsear Coast Floo Flame"
+]
+
+poidsear_coast_locations = [
+    "Tomb of Treachery Floo Flame",
+    "Poidsear Castle Floo Flame",
+    "Phoenix Mountain Cave Floo Flame",
+    "South Poidsear Coast Floo Flame",
+    "Marunweem Bridge Floo Flame"
+]
+
+marunweem_lake_locations = [
+    "Coastal Mine Floo Flame",
+    "Tower Tunnel Floo Flame",
+    "Marunweem Lake Floo Flame",
+    "Marunweem Floo Flame",
+    "Marunweem Ruins Floo Flame"
+]
+
+cragcroftshire_locations = [
+    "Cragcroft Shore Floo Flame",
+    "Cragcroft Floo Flame"
+]
+
+manor_cape_locations = [
+    "Bainburgh Floo Flame",
+    "West Manor Cape Floo Flame"
+]
+
+clagmar_coast_locations = [
+    "Clagmar Castle Floo Flame",
+    "South Clagmar Coast Floo Flame"
+]
