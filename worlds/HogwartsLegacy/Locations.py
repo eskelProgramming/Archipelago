26b85d1beb1c5b55312d459b83560f82cf3b78ca
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2026-01-15 14:20:41 -0800

    Refactor Locations.py
    Refactored to allow for each location type to individually be added. This is because the player will have the option to configure specific location type in the Options page.

--- worlds/HogwartsLegacy/Locations.py
+++ worlds/HogwartsLegacy/Locations.py
@@ -1,164 +1,165 @@
+from typing import Dict, List
+
 from BaseClasses import Location
 
 
 class HogwartsLegacyLocation(Location):
     game: str = "Hogwarts Legacy"
 
-hogwarts_castle_locations = [
-    "Astronomy Tower Floo Flame",
-    "Charms Classroom Floo Flame",
-    "Defence Against the Dark Arts Tower Floo Flame",
-    "Defense Against the Dark Arts Classroom Floo Flame",
-    "Professor Fig’s Classroom Floo Flame",
-    "Transfiguration Classroom Floo Flame",
-    "Transfiguration Courtyard Floo Flame",
-    "Bell Tower Courtyard Floo Flame",
-    "Flying Class Lawn Floo Flame",
-    "Beasts Classroom Floo Flame",
-    "Hogwarts North Exit Floo Flame",
-    "West Tower Floo Flame",
-    "Grand Staircase Floo Flame",
-    "Grand Staircase Tower Floo Flame",
-    "Lower Grand Staircase Floo Flame",
-    "Quad Courtyard Floo Flame",
-    "Ravenclaw Tower Floo Flame",
-    "Trophy Room Floo Flame",
-    "Boathouse Floo Flame",
-    "Great Hall Floo Flame",
-    "Viaduct Courtyard Floo Flame",
-    "Central Hall Floo Flame",
-    "Divination Classroom Floo Flame",
-    "Greenhouses Floo Flame",
-    "Library Floo Flame",
-    "Potions Classroom Floo Flame",
-    "The Map Chamber Floo Flame",
-    "Room of Requirement Floo Flame",
-    "Clock Tower Courtyard Floo Flame",
-    "Faculty Tower Floo Flame",
-    "Gryffindor Common Room Floo Flame",
-    "Hospital Wing Floo Flame", # TODO: Double check if accessible
-    "Hogwarts South Exit Floo Flame"
-]
+floo_flame_locations: Dict[str, List[str]] = {
+    "hogwarts_castle" : [
+        "Astronomy Tower Floo Flame",
+        "Charms Classroom Floo Flame",
+        "Defence Against the Dark Arts Tower Floo Flame",
+        "Defense Against the Dark Arts Classroom Floo Flame",
+        "Professor Fig’s Classroom Floo Flame",
+        "Transfiguration Classroom Floo Flame",
+        "Transfiguration Courtyard Floo Flame",
+        "Bell Tower Courtyard Floo Flame",
+        "Flying Class Lawn Floo Flame",
+        "Beasts Classroom Floo Flame",
+        "Hogwarts North Exit Floo Flame",
+        "West Tower Floo Flame",
+        "Grand Staircase Floo Flame",
+        "Grand Staircase Tower Floo Flame",
+        "Lower Grand Staircase Floo Flame",
+        "Quad Courtyard Floo Flame",
+        "Ravenclaw Tower Floo Flame",
+        "Trophy Room Floo Flame",
+        "Boathouse Floo Flame",
+        "Great Hall Floo Flame",
+        "Viaduct Courtyard Floo Flame",
+        "Central Hall Floo Flame",
+        "Divination Classroom Floo Flame",
+        "Greenhouses Floo Flame",
+        "Library Floo Flame",
+        "Potions Classroom Floo Flame",
+        "The Map Chamber Floo Flame",
+        "Room of Requirement Floo Flame",
+        "Clock Tower Courtyard Floo Flame",
+        "Faculty Tower Floo Flame",
+        "Gryffindor Common Room Floo Flame",
+        "Hospital Wing Floo Flame", # TODO: Double check if accessible
+        "Hogwarts South Exit Floo Flame"
+    ],
+    "hogsmeade" : [
+        "South Hogsmeade Floo Flame",
+        "West Hogsmeade Floo Flame",
+        "North Hogsmeade Floo Flame"
+    ],
+    "south_hogwarts" : [
+        "Mooncalf Den Floo Flame",
+        "Aranshire Floo Flame",
+        "Lower Hogsfield Floo Flame"
+    ],
+    "north_hogwarts" : [
+        "The Collector’s Cave Floo Flame",
+        "East North Hogwarts Region Floo Flame",
+        "Forbidden Forest Floo Flame",
+        "Korrow Ruins Floo Flame"
+    ],
+    "hogsmeade_valley" : [
+        "Upper Hogsfield Floo Flame",
+        "East Hogsmeade Valley Floo Flame",
+        "Falbarton Castle Floo Flame"
+    ],
+    "forbidden_forrest" : [
+        "West Forbidden Forest Floo Flame",
+        "Jackdaw’s Tomb Floo Flame"
+    ],
+    "north_ford_bog" : [
+        "Pitt-Upon-Ford Floo Flame",
+        "North Ford Bog Floo Flame",
+        "San Bakar’s Tower Floo Flame",
+        "East North Ford Bog Floo Flame"
+    ],
+    "hogwarts_valley" : [
+        "Central Hogwarts Valley Floo Flame",
+        "Brocburrow Floo Flame",
+        "Keenbridge Floo Flame",
+        "The Mine’s Eye Floo Flame"
+    ],
+    "feldcroft" : [
+        "West Hogwarts Valley Floo Flame",
+        "North Feldcroft Floo Flame",
+        "Rookwood Castle Floo Flame",
+        "Feldcroft Floo Flame",
+        "South Feldcroft Floo Flame",
+        "Feldcroft Catacomb Floo Flame",
+        "Irondale Floo Flame"
+    ],
+    "south_sea_bog" : [
+        "Northern South Sea Bog Floo Flame"
+    ],
+    "coastal_cavern" : [
+        "East South Sea Bog Floo Flame",
+        "North Poidsear Coast Floo Flame"
+    ],
+    "poidsear_coast" : [
+        "Tomb of Treachery Floo Flame",
+        "Poidsear Castle Floo Flame",
+        "Phoenix Mountain Cave Floo Flame",
+        "South Poidsear Coast Floo Flame",
+        "Marunweem Bridge Floo Flame"
+    ],
+    "marunweem_lake" : [
+        "Coastal Mine Floo Flame",
+        "Tower Tunnel Floo Flame",
+        "Marunweem Lake Floo Flame",
+        "Marunweem Floo Flame",
+        "Marunweem Ruins Floo Flame"
+    ],
+    "cragcrosftshire" : [
+        "Cragcroft Shore Floo Flame",
+        "Cragcroft Floo Flame"
+    ],
+    "manor_cape" : [
+        "Bainburgh Floo Flame",
+        "West Manor Cape Floo Flame"
+    ],
+    "clagmar_coast" : [
+        "Clagmar Castle Floo Flame",
+        "South Clagmar Coast Floo Flame"
+    ]
+}
 
-hogmseade_locations = [
-    "South Hogsmeade Floo Flame",
-    "West Hogsmeade Floo Flame",
-    "North Hogsmeade Floo Flame"
-]
-
-south_hogwarts_locations = [
-    "Mooncalf Den Floo Flame",
-    "Aranshire Floo Flame",
-    "Lower Hogsfield Floo Flame"
-]
-
-north_hogwarts_locations = [
-    "The Collector’s Cave Floo Flame",
-    "East North Hogwarts Region Floo Flame",
-    "Forbidden Forest Floo Flame",
-    "Korrow Ruins Floo Flame",
-    "North Hogwarts Region Ancient Magic Hotspot",
-
-]
-
-hogsmeade_valley_locations = [
-    "Upper Hogsfield Floo Flame",
-    "East Hogsmeade Valley Floo Flame",
-    "Falbarton Castle Floo Flame",
-    "Hogsmeade Valley Ancient Magic Hotspot",
-
-]
-
-forbidden_forrest_locations = [
-    "West Forbidden Forest Floo Flame",
-    "Jackdaw’s Tomb Floo Flame",
-    "Forbidden Forest Ancient Magic Hotspot #1",
-    "Forbidden Forest Ancient Magic Hotspot #2",
-
-]
-
-north_ford_bog_locations = [
-    "Pitt-Upon-Ford Floo Flame",
-    "North Ford Bog Floo Flame",
-    "San Bakar’s Tower Floo Flame",
-    "East North Ford Bog Floo Flame",
-]
-
-hogwarts_valley_locations = [
-    "Central Hogwarts Valley Floo Flame",
-    "Brocburrow Floo Flame",
-    "Keenbridge Floo Flame",
-    "The Mine’s Eye Floo Flame",
-    "Hogwarts Valley Ancient Magic Hotspot #1",
-    "Hogwarts Valley Ancient Magic Hotspot #2",
-    "Hogwarts Valley Ancient Magic Hotspot #3",
-    "Hogwarts Valley Ancient Magic Hotspot #4",
-    "Hogwarts Valley Ancient Magic Hotspot #5",
-    "Hogwarts Valley Ancient Magic Hotspot #6",
-
-]
-
-feldcroft_locations = [
-    "West Hogwarts Valley Floo Flame",
-    "North Feldcroft Floo Flame",
-    "Rookwood Castle Floo Flame",
-    "Feldcroft Floo Flame",
-    "South Feldcroft Floo Flame",
-    "Feldcroft Catacomb Floo Flame",
-    "Irondale Floo Flame",
-    "Feldcroft Region Ancient Magic Hotspot #1",
-    "Feldcroft Region Ancient Magic Hotspot #2",
-    "Feldcroft Region Ancient Magic Hotspot #3",
-    "Feldcroft Region Ancient Magic Hotspot #4",
-
-]
-
-south_sea_bog_locations = [
-    "Northern South Sea Bog Floo Flame"
-]
-
-costal_cavern_locations = [
-    "East South Sea Bog Floo Flame",
-    "North Poidsear Coast Floo Flame"
-]
-
-poidsear_coast_locations = [
-    "Tomb of Treachery Floo Flame",
-    "Poidsear Castle Floo Flame",
-    "Phoenix Mountain Cave Floo Flame",
-    "South Poidsear Coast Floo Flame",
-    "Marunweem Bridge Floo Flame",
-    "Poidsear Coast Ancient Magic Hotspot #1",
-    "Poidsear Coast Ancient Magic Hotspot #2",
-
-]
-
-marunweem_lake_locations = [
-    "Coastal Mine Floo Flame",
-    "Tower Tunnel Floo Flame",
-    "Marunweem Lake Floo Flame",
-    "Marunweem Floo Flame",
-    "Marunweem Ruins Floo Flame",
-    "Marunweem Lake Ancient Magic Hotspot #1",
-    "Marunweem Lake Ancient Magic Hotspot #2",
-
-]
-
-cragcroftshire_locations = [
-    "Cragcroft Shore Floo Flame",
-    "Cragcroft Floo Flame",
-    "Cragcroftshire Ancient Magic Hotspot",
-
-]
-
-manor_cape_locations = [
-    "Bainburgh Floo Flame",
-    "West Manor Cape Floo Flame",
-    "Manor Cape Ancient Magic Hotspot",
-
-]
-
-clagmar_coast_locations = [
-    "Clagmar Castle Floo Flame",
-    "South Clagmar Coast Floo Flame"
-]
+ancient_magic_hotspots: Dict[str, List[str]] = {
+    "north_hogwarts" : [
+        "North Hogwarts Region Ancient Magic Hotspot"
+    ],
+    "hogsmeade_valley" : [
+        "Hogsmeade Valley Ancient Magic Hotspot"
+    ],
+    "forbidden_forrest" : [
+        "Forbidden Forest Ancient Magic Hotspot #1",
+        "Forbidden Forest Ancient Magic Hotspot #2"
+    ],
+    "hogwarts_valley" : [
+        "Hogwarts Valley Ancient Magic Hotspot #1",
+        "Hogwarts Valley Ancient Magic Hotspot #2",
+        "Hogwarts Valley Ancient Magic Hotspot #3",
+        "Hogwarts Valley Ancient Magic Hotspot #4",
+        "Hogwarts Valley Ancient Magic Hotspot #5",
+        "Hogwarts Valley Ancient Magic Hotspot #6"
+    ],
+    "feldcroft" : [
+        "Feldcroft Region Ancient Magic Hotspot #1",
+        "Feldcroft Region Ancient Magic Hotspot #2",
+        "Feldcroft Region Ancient Magic Hotspot #3",
+        "Feldcroft Region Ancient Magic Hotspot #4"
+    ],
+    "poidsear_coast" : [
+        "Poidsear Coast Ancient Magic Hotspot #1",
+        "Poidsear Coast Ancient Magic Hotspot #2"
+    ],
+    "marunweem_lake" : [
+        "Marunweem Lake Ancient Magic Hotspot #1",
+        "Marunweem Lake Ancient Magic Hotspot #2"
+    ],
+    "cragcrosftshire" : [
+        "CragcrosftShire Ancient Magic Hotspot"
+    ],
+    "manor_cape" : [
+        "Manor Cape Ancient Magic Hotspot"
+    ]
+}
