b13057f6d3a8e4991a722251e216eb6359877b5f
Emily Skelley <166662972+eskelProgramming@users.noreply.github.com>
2026-01-22 11:02:03 -0800

    Add Spells to Items

---
+++ worlds/hogwarts_legacy/Items.py
@@ -0,0 +1,150 @@
+from typing import TypedDict, List
+
+from BaseClasses import Item, ItemClassification
+
+
+class HogwartsLegacyItem(Item):
+    name: str = "Hogwarts Legacy"
+
+class ItemDict(TypedDict):
+    name: str
+    count: int
+    classification: ItemClassification
+
+spells: List[ItemDict] = [
+    {
+        "name": "Arresto Momentum",
+        "count": 1,
+        "classification": ItemClassification.progression
+    },
+    {
+        "name": "Glacius",
+        "count": 1,
+        "classification": ItemClassification.progression
+    },
+    {
+        "name": "Levioso",
+        "count": 1,
+        "classification": ItemClassification.progression
+    },
+    {
+        "name": "Transformation",
+        "count": 1,
+        "classification": ItemClassification.progression
+    },
+    {
+        "name": "Accio",
+        "count": 1,
+        "classification": ItemClassification.progression
+    },
+    {
+        "name": "Depulso",
+        "count": 1,
+        "classification": ItemClassification.progression
+    },
+    {
+        "name": "Descendor",
+        "count": 1,
+        "classification": ItemClassification.progression
+    },
+    {
+        "name": "Flipendo",
+        "count": 1,
+        "classification": ItemClassification.progression
+    },
+    {
+        "name": "Confringo",
+        "count": 1,
+        "classification": ItemClassification.progression
+    },
+    {
+        "name": "Diffindo",
+        "count": 1,
+        "classification": ItemClassification.progression
+    },
+    {
+        "name": "Expelliarmus",
+        "count": 1,
+        "classification": ItemClassification.progression
+    },
+    {
+        "name": "Bombarda",
+        "count": 1,
+        "classification": ItemClassification.progression
+    },
+    {
+        "name": "Incendio",
+        "count": 1,
+        "classification": ItemClassification.progression
+    },
+    {
+        "name": "Disillusionment",
+        "count": 1,
+        "classification": ItemClassification.progression
+    },
+    {
+        "name": "Reparo",
+        "count": 1,
+        "classification": ItemClassification.progression
+    },
+    {
+        "name": "Wingardium Leviosa",
+        "count": 1,
+        "classification": ItemClassification.progression
+    },
+    {
+        "name": "Conjuring Spell",
+        "count": 1,
+        "classification": ItemClassification.useful
+    },
+    {
+        "name": "Altering Spell",
+        "count": 1,
+        "classification": ItemClassification.useful
+    },
+    {
+        "name": "Avada Kedavra",
+        "count": 1,
+        "classification": ItemClassification.useful
+    },
+    {
+        "name": "Crucio",
+        "count": 1,
+        "classification": ItemClassification.useful
+    },
+    {
+        "name": "Imperio",
+        "count": 1,
+        "classification": ItemClassification.useful
+    },
+    {
+        "name": "Beast Petting Brush",
+        "count": 1,
+        "classification": ItemClassification.useful
+    },
+    {
+        "name": "Beast Feed",
+        "count": 1,
+        "classification": ItemClassification.useful
+    },
+    {
+        "name": "Nab-Sack",
+        "count": 1,
+        "classification": ItemClassification.useful
+    },
+    {
+        "name": "Ancient Magic Throw",
+        "count": 1,
+        "classification": ItemClassification.progression
+    },
+    {
+        "name": "Ancient Magic",
+        "count": 1,
+        "classification": ItemClassification.progression
+    },
+    {
+        "name": "Alohomora",
+        "count": 1,
+        "classification": ItemClassification.progression
+    }
+]
