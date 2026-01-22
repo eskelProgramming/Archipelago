bbc8f4b3e5c253f5fd121f8aeab94b65cab491b4
Emily Skelley <166662972+eskelProgramming@users.noreply.github.com>
2026-01-22 12:16:57 -0800

    Added Items

--- worlds/hogwarts_legacy/Items.py
+++ worlds/hogwarts_legacy/Items.py
@@ -148,3 +148,168 @@ spells: List[ItemDict] = [
         "classification": ItemClassification.progression
     }
 ]
+
+key_items = [
+    {
+        "name": "Pensieve Artifact",
+        "count": 6,
+        "classification": ItemClassification.progression_skip_balancing
+    },
+    {
+        "name": "Telescope",
+        "count": 1,
+        "classification": ItemClassification.progression
+    },
+    {
+        "name": "Broom",
+        "count": 1,
+        "classification": ItemClassification.progression
+    }
+]
+
+non_required_quest_items = [
+    {
+        "name": "Demiguise Moon",
+        "count": 30,
+        "classification": ItemClassification.useful
+    },
+    {
+        "name": "House Token",
+        "count": 16,
+        "classification": ItemClassification.useful
+    }
+]
+
+potion_recipes_items = [
+    {
+        "name": "Wiggenweld Potion Recipe",
+        "count": 1,
+        "classification": ItemClassification.progression # Required for Potions Class
+    },
+    {
+        "name": "Focus Potion Recipe",
+        "count": 1,
+        "classification": ItemClassification.useful
+    },
+    {
+        "name": "Edurus Potion Recipe",
+        "count": 1,
+        "classification": ItemClassification.useful
+    },
+    {
+        "name": "Invisibility Potion Recipe",
+        "count": 1,
+        "classification": ItemClassification.useful
+    },
+    {
+        "name": "Maxima Potion Recipe",
+        "count": 1,
+        "classification": ItemClassification.useful
+    },
+    {
+        "name": "Thunderbrew Potion Recipe",
+        "count": 1,
+        "classification": ItemClassification.useful
+    }
+]
+
+seed_items = [
+    {
+        "name": "Knotgrass Seed",
+        "count": 1,
+        "classification": ItemClassification.useful
+    },
+    {
+        "name": "Dittany Seed",
+        "count": 1,
+        "classification": ItemClassification.progression # required for Herbology Class
+    },
+    {
+        "name": "Mallowsweet Seed",
+        "count": 1,
+        "classification": ItemClassification.progression # required for Merlin Trials
+    },
+    {
+        "name": "Fluxweed Seed",
+        "count": 1,
+        "classification": ItemClassification.useful
+    },
+    {
+        "name": "Shrivelfig Seed",
+        "count": 1,
+        "classification": ItemClassification.useful
+    },
+    {
+        "name": "Chinese Chomping Cabbage Seed",
+        "count": 1,
+        "classification": ItemClassification.useful
+    },
+    {
+        "name": "Mandrake Seed",
+        "count": 1,
+        "classification": ItemClassification.useful
+    },
+    {
+        "name": "Venomous Tentacula Seed",
+        "count": 1,
+        "classification": ItemClassification.useful
+    }
+]
+
+filler_items = [
+    {
+        "name": "Galleons",
+        "count": 1,
+        "classification": ItemClassification.filler
+    },
+    {
+        "name": "Wiggenweld Potions",
+        "count": 1,
+        "classification": ItemClassification.filler
+    },
+    {
+        "name": "Chinese Chomping Cabbage",
+        "count": 1,
+        "classification": ItemClassification.filler
+    },
+    {
+        "name": "Focus Potion",
+        "count": 1,
+        "classification": ItemClassification.filler
+    },
+    {
+        "name": "Edurus Potion",
+        "count": 1,
+        "classification": ItemClassification.filler
+    },
+    {
+        "name": "Invisibility Potion",
+        "count": 1,
+        "classification": ItemClassification.filler
+    },
+    {
+        "name": "Mandrake",
+        "count": 1,
+        "classification": ItemClassification.filler
+    },
+    {
+        "name": "Maxima Potion",
+        "count": 1,
+        "classification": ItemClassification.filler
+    },
+    {
+        "name": "Thunderbrew Potion",
+        "count": 1,
+        "classification": ItemClassification.filler
+    },
+    {
+        "name" : "Venomous Tentacula",
+        "count": 1,
+        "classification": ItemClassification.filler
+    },
+    {
+        "name": "Mallowsweet",
+        "count": 1,
+        "classification": ItemClassification.filler
+    }
+]
