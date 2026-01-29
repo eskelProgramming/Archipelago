0f8b3487e546ea2b6284fa73f0bd3c3a0361caf5
Emily Skelley <166662972+eskelProgramming@users.noreply.github.com>
2026-01-29 13:59:43 -0800

    Created Basic Rules

--- worlds/hogwarts_legacy/Rules.py
+++ worlds/hogwarts_legacy/Rules.py
@@ -18,16 +18,7 @@ class HogwartsLegacyRules:
     def __init__(self, world: HogwartsLegacyWorld) -> None:
         self.player = world.player
         self.world = world
-        for location in ancient_magic_hotspots:
-            self.location_rules[location] = self.has_ancient_magic
-        for location in merlin_trial_locations:
-            self.location_rules[location] = self.has_mallowsweet
-        for location in astronomy_table_locations:
-            self.location_rules[location] = self.has_telescope
-        for location in balloon_locations:
-            self.location_rules[location] = self.has_broom
-        for location in broom_trial_locations:
-            self.location_rules[location] = self.has_broom
+
 
     def has_ancient_magic(self, state: CollectionState) -> bool:
         return state.has("Ancient Magic", self.player)
@@ -40,3 +31,26 @@ class HogwartsLegacyRules:
 
     def has_broom(self, state: CollectionState) -> bool:
         return state.has("Broom", self.player)
+
+    def has_alohomora_one(self, state: CollectionState) -> bool:
+        return state.has("Progressive Alohomora", self.player, 1)
+
+    def has_alohomora_two(self, state: CollectionState) -> bool:
+        return state.has("Progressive Alohomora", self.player, 2)
+
+    def has_alohomora_three(self, state: CollectionState) -> bool:
+        return state.has("Progressive Alohomora", self.player, 3)
+
+    def has_lumos(self, state: CollectionState) -> bool:
+        return state.has("Lumos", self.player)
+
+    def has_accio(self, state: CollectionState) -> bool:
+        return state.has("Accio", self.player)
+
+    def has_levioso(self, state: CollectionState) -> bool:
+        return state.has("Levioso", self.player)
+
+    def has_incendio(self, state: CollectionState) -> bool:
+        return state.has("Incendio", self.player)
+
+    
