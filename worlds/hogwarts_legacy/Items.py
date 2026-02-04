fe906f98f31c67cb80058560ab84183dfc162cc0
Emily Skelley <166662972+eskelProgramming@users.noreply.github.com>
2026-02-04 12:08:16 -0800

    Testing environment created
    Reduced the number of locations and items to make testing easier.

--- worlds/hogwarts_legacy/Items.py
+++ worlds/hogwarts_legacy/Items.py
@@ -17,143 +17,143 @@ spells: List[ItemDict] = [
         "name": "Arresto Momentum",
         "count": 1,
         "classification": ItemClassification.progression
-    },
-    {
-        "name": "Glacius",
-        "count": 1,
-        "classification": ItemClassification.progression
-    },
-    {
-        "name": "Levioso",
-        "count": 1,
-        "classification": ItemClassification.progression
-    },
-    {
-        "name": "Transformation",
-        "count": 1,
-        "classification": ItemClassification.progression
-    },
-    {
-        "name": "Accio",
-        "count": 1,
-        "classification": ItemClassification.progression
-    },
-    {
-        "name": "Depulso",
-        "count": 1,
-        "classification": ItemClassification.progression
-    },
-    {
-        "name": "Descendor",
-        "count": 1,
-        "classification": ItemClassification.progression
-    },
-    {
-        "name": "Flipendo",
-        "count": 1,
-        "classification": ItemClassification.progression
-    },
-    {
-        "name": "Confringo",
-        "count": 1,
-        "classification": ItemClassification.progression
-    },
-    {
-        "name": "Diffindo",
-        "count": 1,
-        "classification": ItemClassification.progression
-    },
-    {
-        "name": "Expelliarmus",
-        "count": 1,
-        "classification": ItemClassification.progression
-    },
-    {
-        "name": "Bombarda",
-        "count": 1,
-        "classification": ItemClassification.progression
-    },
-    {
-        "name": "Incendio",
-        "count": 1,
-        "classification": ItemClassification.progression
-    },
-    {
-        "name": "Disillusionment",
-        "count": 1,
-        "classification": ItemClassification.progression
-    },
-    {
-        "name": "Reparo",
-        "count": 1,
-        "classification": ItemClassification.progression
-    },
-    {
-        "name": "Wingardium Leviosa",
-        "count": 1,
-        "classification": ItemClassification.progression
-    },
-    {
-        "name": "Conjuring Spell",
-        "count": 1,
-        "classification": ItemClassification.useful
-    },
-    {
-        "name": "Altering Spell",
-        "count": 1,
-        "classification": ItemClassification.useful
-    },
-    {
-        "name": "Avada Kedavra",
-        "count": 1,
-        "classification": ItemClassification.useful
-    },
-    {
-        "name": "Crucio",
-        "count": 1,
-        "classification": ItemClassification.useful
-    },
-    {
-        "name": "Imperio",
-        "count": 1,
-        "classification": ItemClassification.useful
-    },
-    {
-        "name": "Beast Petting Brush",
-        "count": 1,
-        "classification": ItemClassification.useful
-    },
-    {
-        "name": "Beast Feed",
-        "count": 1,
-        "classification": ItemClassification.useful
-    },
-    {
-        "name": "Nab-Sack",
-        "count": 1,
-        "classification": ItemClassification.useful
-    },
-    {
-        "name": "Ancient Magic Throw",
-        "count": 1,
-        "classification": ItemClassification.progression
-    },
-    {
-        "name": "Ancient Magic",
-        "count": 1,
-        "classification": ItemClassification.progression
-    },
-    {
-        "name": "Progressive Alohomora",
-        "count": 3,
-        "classification": ItemClassification.progression
     }
+    # {
+    #     "name": "Glacius",
+    #     "count": 1,
+    #     "classification": ItemClassification.progression
+    # },
+    # {
+    #     "name": "Levioso",
+    #     "count": 1,
+    #     "classification": ItemClassification.progression
+    # },
+    # {
+    #     "name": "Transformation",
+    #     "count": 1,
+    #     "classification": ItemClassification.progression
+    # },
+    # {
+    #     "name": "Accio",
+    #     "count": 1,
+    #     "classification": ItemClassification.progression
+    # },
+    # {
+    #     "name": "Depulso",
+    #     "count": 1,
+    #     "classification": ItemClassification.progression
+    # },
+    # {
+    #     "name": "Descendor",
+    #     "count": 1,
+    #     "classification": ItemClassification.progression
+    # },
+    # {
+    #     "name": "Flipendo",
+    #     "count": 1,
+    #     "classification": ItemClassification.progression
+    # },
+    # {
+    #     "name": "Confringo",
+    #     "count": 1,
+    #     "classification": ItemClassification.progression
+    # },
+    # {
+    #     "name": "Diffindo",
+    #     "count": 1,
+    #     "classification": ItemClassification.progression
+    # },
+    # {
+    #     "name": "Expelliarmus",
+    #     "count": 1,
+    #     "classification": ItemClassification.progression
+    # },
+    # {
+    #     "name": "Bombarda",
+    #     "count": 1,
+    #     "classification": ItemClassification.progression
+    # },
+    # {
+    #     "name": "Incendio",
+    #     "count": 1,
+    #     "classification": ItemClassification.progression
+    # },
+    # {
+    #     "name": "Disillusionment",
+    #     "count": 1,
+    #     "classification": ItemClassification.progression
+    # },
+    # {
+    #     "name": "Reparo",
+    #     "count": 1,
+    #     "classification": ItemClassification.progression
+    # },
+    # {
+    #     "name": "Wingardium Leviosa",
+    #     "count": 1,
+    #     "classification": ItemClassification.progression
+    # },
+    # {
+    #     "name": "Conjuring Spell",
+    #     "count": 1,
+    #     "classification": ItemClassification.useful
+    # },
+    # {
+    #     "name": "Altering Spell",
+    #     "count": 1,
+    #     "classification": ItemClassification.useful
+    # },
+    # {
+    #     "name": "Avada Kedavra",
+    #     "count": 1,
+    #     "classification": ItemClassification.useful
+    # },
+    # {
+    #     "name": "Crucio",
+    #     "count": 1,
+    #     "classification": ItemClassification.useful
+    # },
+    # {
+    #     "name": "Imperio",
+    #     "count": 1,
+    #     "classification": ItemClassification.useful
+    # },
+    # {
+    #     "name": "Beast Petting Brush",
+    #     "count": 1,
+    #     "classification": ItemClassification.useful
+    # },
+    # {
+    #     "name": "Beast Feed",
+    #     "count": 1,
+    #     "classification": ItemClassification.useful
+    # },
+    # {
+    #     "name": "Nab-Sack",
+    #     "count": 1,
+    #     "classification": ItemClassification.useful
+    # },
+    # {
+    #     "name": "Ancient Magic Throw",
+    #     "count": 1,
+    #     "classification": ItemClassification.progression
+    # },
+    # {
+    #     "name": "Ancient Magic",
+    #     "count": 1,
+    #     "classification": ItemClassification.progression
+    # },
+    # {
+    #     "name": "Progressive Alohomora",
+    #     "count": 3,
+    #     "classification": ItemClassification.progression
+    # }
 ]
 
 goal_items = [
     {
         "name": "Pensieve Artifact",
-        "count": 6,
+        "count": 1,#6,
         "classification": ItemClassification.progression_skip_balancing
     }
 ]
@@ -163,25 +163,25 @@ key_items = [
         "name": "Telescope",
         "count": 1,
         "classification": ItemClassification.progression
-    },
-    {
-        "name": "Broom",
-        "count": 1,
-        "classification": ItemClassification.progression
     }
+    # {
+    #     "name": "Broom",
+    #     "count": 1,
+    #     "classification": ItemClassification.progression
+    # }
 ]
 
 non_required_quest_items = [
     {
         "name": "Demiguise Moon",
-        "count": 30,
-        "classification": ItemClassification.useful
-    },
-    {
-        "name": "House Token",
-        "count": 16,
+        "count": 1,#30,
         "classification": ItemClassification.useful
     }
+    # {
+    #     "name": "House Token",
+    #     "count": 16,
+    #     "classification": ItemClassification.useful
+    # }
 ]
 
 potion_recipes_items = [
@@ -189,32 +189,32 @@ potion_recipes_items = [
         "name": "Wiggenweld Potion Recipe",
         "count": 1,
         "classification": ItemClassification.progression # Required for Potions Class
-    },
-    {
-        "name": "Focus Potion Recipe",
-        "count": 1,
-        "classification": ItemClassification.useful
-    },
-    {
-        "name": "Edurus Potion Recipe",
-        "count": 1,
-        "classification": ItemClassification.useful
-    },
-    {
-        "name": "Invisibility Potion Recipe",
-        "count": 1,
-        "classification": ItemClassification.useful
-    },
-    {
-        "name": "Maxima Potion Recipe",
-        "count": 1,
-        "classification": ItemClassification.useful
-    },
-    {
-        "name": "Thunderbrew Potion Recipe",
-        "count": 1,
-        "classification": ItemClassification.useful
     }
+    # {
+    #     "name": "Focus Potion Recipe",
+    #     "count": 1,
+    #     "classification": ItemClassification.useful
+    # },
+    # {
+    #     "name": "Edurus Potion Recipe",
+    #     "count": 1,
+    #     "classification": ItemClassification.useful
+    # },
+    # {
+    #     "name": "Invisibility Potion Recipe",
+    #     "count": 1,
+    #     "classification": ItemClassification.useful
+    # },
+    # {
+    #     "name": "Maxima Potion Recipe",
+    #     "count": 1,
+    #     "classification": ItemClassification.useful
+    # },
+    # {
+    #     "name": "Thunderbrew Potion Recipe",
+    #     "count": 1,
+    #     "classification": ItemClassification.useful
+    # }
 ]
 
 seed_items = [
@@ -222,42 +222,42 @@ seed_items = [
         "name": "Knotgrass Seed",
         "count": 1,
         "classification": ItemClassification.useful
-    },
-    {
-        "name": "Dittany Seed",
-        "count": 1,
-        "classification": ItemClassification.progression # required for Herbology Class
-    },
-    {
-        "name": "Mallowsweet Seed",
-        "count": 1,
-        "classification": ItemClassification.progression # required for Merlin Trials
-    },
-    {
-        "name": "Fluxweed Seed",
-        "count": 1,
-        "classification": ItemClassification.useful
-    },
-    {
-        "name": "Shrivelfig Seed",
-        "count": 1,
-        "classification": ItemClassification.useful
-    },
-    {
-        "name": "Chinese Chomping Cabbage Seed",
-        "count": 1,
-        "classification": ItemClassification.useful
-    },
-    {
-        "name": "Mandrake Seed",
-        "count": 1,
-        "classification": ItemClassification.useful
-    },
-    {
-        "name": "Venomous Tentacula Seed",
-        "count": 1,
-        "classification": ItemClassification.useful
     }
+    # {
+    #     "name": "Dittany Seed",
+    #     "count": 1,
+    #     "classification": ItemClassification.progression # required for Herbology Class
+    # },
+    # {
+    #     "name": "Mallowsweet Seed",
+    #     "count": 1,
+    #     "classification": ItemClassification.progression # required for Merlin Trials
+    # },
+    # {
+    #     "name": "Fluxweed Seed",
+    #     "count": 1,
+    #     "classification": ItemClassification.useful
+    # },
+    # {
+    #     "name": "Shrivelfig Seed",
+    #     "count": 1,
+    #     "classification": ItemClassification.useful
+    # },
+    # {
+    #     "name": "Chinese Chomping Cabbage Seed",
+    #     "count": 1,
+    #     "classification": ItemClassification.useful
+    # },
+    # {
+    #     "name": "Mandrake Seed",
+    #     "count": 1,
+    #     "classification": ItemClassification.useful
+    # },
+    # {
+    #     "name": "Venomous Tentacula Seed",
+    #     "count": 1,
+    #     "classification": ItemClassification.useful
+    # }
 ]
 
 filler_items = [
@@ -265,55 +265,55 @@ filler_items = [
         "name": "Galleons",
         "count": 1,
         "classification": ItemClassification.filler
-    },
-    {
-        "name": "Wiggenweld Potions",
-        "count": 1,
-        "classification": ItemClassification.filler
-    },
-    {
-        "name": "Chinese Chomping Cabbage",
-        "count": 1,
-        "classification": ItemClassification.filler
-    },
-    {
-        "name": "Focus Potion",
-        "count": 1,
-        "classification": ItemClassification.filler
-    },
-    {
-        "name": "Edurus Potion",
-        "count": 1,
-        "classification": ItemClassification.filler
-    },
-    {
-        "name": "Invisibility Potion",
-        "count": 1,
-        "classification": ItemClassification.filler
-    },
-    {
-        "name": "Mandrake",
-        "count": 1,
-        "classification": ItemClassification.filler
-    },
-    {
-        "name": "Maxima Potion",
-        "count": 1,
-        "classification": ItemClassification.filler
-    },
-    {
-        "name": "Thunderbrew Potion",
-        "count": 1,
-        "classification": ItemClassification.filler
-    },
-    {
-        "name" : "Venomous Tentacula",
-        "count": 1,
-        "classification": ItemClassification.filler
-    },
-    {
-        "name": "Mallowsweet",
-        "count": 1,
-        "classification": ItemClassification.filler
     }
+    # {
+    #     "name": "Wiggenweld Potions",
+    #     "count": 1,
+    #     "classification": ItemClassification.filler
+    # },
+    # {
+    #     "name": "Chinese Chomping Cabbage",
+    #     "count": 1,
+    #     "classification": ItemClassification.filler
+    # },
+    # {
+    #     "name": "Focus Potion",
+    #     "count": 1,
+    #     "classification": ItemClassification.filler
+    # },
+    # {
+    #     "name": "Edurus Potion",
+    #     "count": 1,
+    #     "classification": ItemClassification.filler
+    # },
+    # {
+    #     "name": "Invisibility Potion",
+    #     "count": 1,
+    #     "classification": ItemClassification.filler
+    # },
+    # {
+    #     "name": "Mandrake",
+    #     "count": 1,
+    #     "classification": ItemClassification.filler
+    # },
+    # {
+    #     "name": "Maxima Potion",
+    #     "count": 1,
+    #     "classification": ItemClassification.filler
+    # },
+    # {
+    #     "name": "Thunderbrew Potion",
+    #     "count": 1,
+    #     "classification": ItemClassification.filler
+    # },
+    # {
+    #     "name" : "Venomous Tentacula",
+    #     "count": 1,
+    #     "classification": ItemClassification.filler
+    # },
+    # {
+    #     "name": "Mallowsweet",
+    #     "count": 1,
+    #     "classification": ItemClassification.filler
+    # }
 ]
